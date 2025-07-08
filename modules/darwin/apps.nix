cca6547b58c447c9571358887df32d5d4a492e98
hest <henrik.starefors@gmail.com>
2025-07-08 14:05:38 +0200

    mender artifact + update

--- modules/darwin/apps.nix
+++ modules/darwin/apps.nix
@@ -150,6 +150,7 @@
       "curl" # no not install curl via nixpkgs, it's not working well on macOS!
       "lima-additional-guestagents"
       "colima"
+      "mender-artifact"
     ];
 
     casks = [
