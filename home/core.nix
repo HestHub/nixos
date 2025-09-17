0ec4ded178387fd2389b8c586dd2f11b4d1eff1e
hest <henrik.starefors@gmail.com>
2025-09-17 14:11:32 +0200

    new ssh config struct

--- home/core.nix
+++ home/core.nix
@@ -157,7 +157,19 @@ in {
 
     ssh = {
       enable = true;
-      addKeysToAgent = "yes";
+      enableDefaultConfig = false;
+      matchBlocks."*" = {
+        forwardAgent = false;
+        addKeysToAgent = "yes";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
     };
 
     ripgrep.enable = true;
