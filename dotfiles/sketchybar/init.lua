1b4cb7adadd66cadaf1ef1cfc15e14c13f103b79
hest <henrik.starefors@gmail.com>
2025-09-26 13:28:54 +0200

    remove comments

--- dotfiles/sketchybar/init.lua
+++ dotfiles/sketchybar/init.lua
@@ -1,10 +1,5 @@
--- Require the sketchybar module
 sbar = require("sketchybar")
 
--- Set the bar name, if you are using another bar instance than sketchybar
--- sbar.set_bar_name("bottom_bar")
-
--- Bundle the entire initial configuration into a single message to sketchybar
 sbar.begin_config()
 
 sbar.add("event", "colors_toggled")
@@ -14,15 +9,6 @@ require("bar")
 require("default")
 require("items")
 
--- Add a new item that is running a script
--- sbar.add("item", "clock", "right")
--- sbar.set("clock", {
---   script = "$CONFIG_DIR/scripts/clock.sh",
---   update_freq = 10,
--- })
-
 sbar.end_config()
 
--- Run the event loop of the sketchybar module (without this there will be no
--- callback functions executed in the lua module)
 sbar.event_loop()
