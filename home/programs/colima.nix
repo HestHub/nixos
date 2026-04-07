ac40e7aeb4e668fbc317b7ea6a5d44eaa42c42b6
hest <henrik.starefors@gmail.com>
2026-04-07 19:54:30 +0200

    colima devpod

--- home/programs/colima.nix
+++ home/programs/colima.nix
@@ -1,17 +1,20 @@
-{...}: {
-  xdg.configFile."colima/default/colima.yaml".text = ''
-    cpu: 8
-    memory: 16
-    disk: 100
-    arch: aarch64
-    runtime: docker
-    vmType: vz
-    rosetta: true
-    binfmt: true
-    mountType: virtiofs
-    mountInotify: true
-    autoActivate: true
-    sshConfig: true
-    portForwarder: ssh
+{config, ...}: {
+  home.activation.colimaConfig = config.lib.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p ${config.home.homeDirectory}/.config/colima/default
+    cat > ${config.home.homeDirectory}/.config/colima/default/colima.yaml << 'EOF'
+cpu: 8
+memory: 16
+disk: 100
+arch: aarch64
+runtime: docker
+vmType: vz
+rosetta: true
+binfmt: true
+mountType: virtiofs
+mountInotify: true
+autoActivate: true
+sshConfig: true
+portForwarder: ssh
+EOF
   '';
 }
--- home/programs/devpod.nix
+++ home/programs/devpod.nix
@@ -1,10 +1,10 @@
-{config, ...}: {
+{config, pkgs, ...}: {
   home.activation.devpod = config.lib.dag.entryAfter ["writeBoundary"] ''
-    $DRY_RUN_CMD devpod provider use docker \
+    $DRY_RUN_CMD ${pkgs.devpod}/bin/devpod provider use docker \
       --option DOCKER_HOST=unix://${config.home.homeDirectory}/.config/colima/default/docker.sock \
       --option INACTIVITY_TIMEOUT=20m \
       --silent || true
 
-    $DRY_RUN_CMD devpod ide use none --silent || true
+    $DRY_RUN_CMD ${pkgs.devpod}/bin/devpod ide use none --silent || true
   '';
 }
